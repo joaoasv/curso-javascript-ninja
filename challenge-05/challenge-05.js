ea24cb77c54a78dc4e5d115b18d21373afeae078
João Pedro <jp@asv.digital>
2020-03-13 00:43:01 -0300

    challenge-05 finished

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,22 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+
+var myArr = [true, false, 'string', {a: 10}, null];
+
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+
+function getArr(arr) {
+    return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+getArr(myArr)[1];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +25,23 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+
+function myFunction(arr, num) {
+    return arr[num];
+}
+
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var myVar = [true, false, 'string', {a: 10}, null];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+
+myFunction(myVar, 2);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +57,59 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+
+function book(name) {
+
+    var books = {
+        'JS Ninja': {
+            pages: 35,
+            author: 'João Pedro',
+            lib: 'ASV Digital'
+        },
+        'Design Pattern': {
+            pages: 55,
+            author: 'Fernando Fontes',
+            lib: 'Rabisco Visual'
+        },
+        'Marketing Digital': {
+            pages: 10,
+            author: 'Caio Flores',
+            lib: 'Smart Digital'
+        }
+    }
+
+    if(!name) {
+        return books;
+    }
+
+    return books[name];
+
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log( 'O livro JS Ninja tem ' + book('JS Ninja').pages + ' páginas!' );
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log( 'O autor do livro JS Ninja é ' + book('JS Ninja').author + '!' );
+
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log( 'O livro JS Ninja foi publicado pela editora ' + book('JS Ninja').lib + '!' );
+
